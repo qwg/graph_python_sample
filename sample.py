8109e5d1fd1edf347cf488b05edd609e3c3794bd
Shoji Yoshinori <shoji_yoshinori@my-soft.co.jp>
2021-12-17 16:34:15 +0900

    修正

--- sample.py
+++ sample.py
@@ -6,14 +6,20 @@ import openpyxl
 import scatter
 
 times = ("00:00:00", "03:00:00", "03:00:01", "09:00:15", "02:59:12", "23:59:59")
+data1 = [[datetime.datetime.fromisoformat(f"2021-04-20 {t}.000000"), i] for i, t in enumerate(times)]
+df1 = pd.DataFrame(data1, columns=['date', 'value'])
 
-data = [[datetime.datetime.fromisoformat(f"2021-04-20 {t}.000000"), i] for i, t in enumerate(times)]
+times = ("09:12:03", "02:14:50", "20:00:01", "22:33:15")
+data2 = [[datetime.datetime.fromisoformat(f"2021-04-20 {t}.000000"), i+3] for i, t in enumerate(times)]
+df2 = pd.DataFrame(data2, columns=['date', 'value'])
 
-df = pd.DataFrame(data, columns=['date', 'value'])
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9, 6))
 
-fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(9, 6))
+#scatter.ax_plot(ax, df1['date'].to_list(), df1['value'].to_list(), "a")
+#scatter.ax_plot(ax, df2['date'].to_list(), df2['value'].to_list(), "b")
 
-scatter.plot(df, axes)
+scatter.df_plot(df1, ax, 'date', 'value', "a")
+scatter.df_plot(df2, ax, 'date', 'value', "b")
 
 wb = openpyxl.Workbook()
 ws = wb.active
--- scatter.py
+++ scatter.py
@@ -1,2 +1,8 @@
-def plot(df, ax):
-    df.plot(ax=ax, kind="scatter", x='date', y='value')
+import matplotlib.dates as mdates
+
+def ax_plot(ax, x, y, label):
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    ax.plot(x, y, label=label)
+
+def df_plot(df, ax, x, y, label):
+    df.plot(ax=ax, kind="scatter", x=x, y=y, label=label)
