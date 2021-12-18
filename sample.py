1d43f061616dddcd5b098e4b291e967b8f0b6e31
Shoji Yoshinori <shoji_yoshinori@my-soft.co.jp>
2021-12-18 19:42:04 +0900

    追加

--- sample.py
+++ sample.py
@@ -5,6 +5,11 @@ import io
 import openpyxl
 import scatter
 
+import matplotlib
+ 
+fonts = set([f.name for f in matplotlib.font_manager.fontManager.ttflist])
+print(fonts)
+
 times = ("00:00:00", "03:00:00", "03:00:01", "09:00:15", "02:59:12", "23:59:59")
 data1 = [[datetime.datetime.fromisoformat(f"2021-04-20 {t}.000000"), i] for i, t in enumerate(times)]
 df1 = pd.DataFrame(data1, columns=['date', 'value'])
@@ -13,13 +18,18 @@ times = ("09:12:03", "02:14:50", "20:00:01", "22:33:15")
 data2 = [[datetime.datetime.fromisoformat(f"2021-04-20 {t}.000000"), i+3] for i, t in enumerate(times)]
 df2 = pd.DataFrame(data2, columns=['date', 'value'])
 
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9, 6))
+#fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(9, 6))
 
-#scatter.ax_plot(ax, df1['date'].to_list(), df1['value'].to_list(), "a")
-#scatter.ax_plot(ax, df2['date'].to_list(), df2['value'].to_list(), "b")
+fig = plt.figure()
+ax = fig.add_subplot(111, xlabel='時刻', ylabel='値')
 
-scatter.df_plot(df1, ax, 'date', 'value', "a")
-scatter.df_plot(df2, ax, 'date', 'value', "b")
+scatter.init(ax)
+
+scatter.ax_scatter(ax, df1['date'].to_list(), df1['value'].to_list(), "a")
+scatter.ax_scatter(ax, df2['date'].to_list(), df2['value'].to_list(), "b")
+
+#scatter.df_scatter(df1, ax, 'date', 'value', "a")
+#scatter.df_scatter(df2, ax, 'date', 'value', "b")
 
 wb = openpyxl.Workbook()
 ws = wb.active
--- scatter.py
+++ scatter.py
@@ -1,8 +1,11 @@
 import matplotlib.dates as mdates
 
-def ax_plot(ax, x, y, label):
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-    ax.plot(x, y, label=label)
+def ax_scatter(ax, x, y, label):
+    ax.scatter(x, y, label=label)
 
-def df_plot(df, ax, x, y, label):
+def df_scatter(df, ax, x, y, label):
     df.plot(ax=ax, kind="scatter", x=x, y=y, label=label)
+
+def init(ax):
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    #ax.tick_params(axis='x', rotation=270) x軸のラベルを縦書きする場合
